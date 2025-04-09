2d2a1b7137e980bda9db609beda1501471d37b29
Yuu Murasaki <hoangthibaongoc411@gmail.com>
2025-04-09 21:33:19 +0700

    new

---
+++ install-alpine.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+###################################################################################
+# To make Alpine Linux a rolling release distro,
+# edit the /etc/apk/repositories
+# Change the version --> 'edge'
+# NOTE: For better security you should probably change the url's from http to https
+# Edit: /etc/doas.conf, /etc/passwd
+###################################################################################
+
+# Update system and repositories
+apk update
+apk upgrade --available
+
+# Xorg
+setup-xorg-base xinit xrandr xsetroot xcompmgr xset xclip xwallpaper
+
+# Drivers
+apk add mesa mesa-dri-gallium mesa-va-gallium
+#apk add libva-intel-driver # For Intel < 5th gen
+#apk add intel-media-driver # For Intel >= 5th gen
+#apk add xf86-video-vboxvideo # For Virtualbox
+
+# Libraries
+apk add libx11-dev libxft-dev libxinerama-dev libxrandr-dev
+apk add ncurses linux-headers
+
+# D-bus
+apk add dbus dbus-x11 pam-rundir
+rc-update add dbus default
+setup-devd udev
+
+# Programming tools
+apk add alpine-sdk curl
+apk add lua5.1 luarocks luajit
+#apk add nodejs npm tree-sitter-cli
+
+# Wifi
+#apk add iwd
+#apk add networkmanager networkmanager-tui
+
+# Sound
+apk add pipewire wireplumber pipewire-pulse pipewire-jack
+
+# Cli tools
+apk add unclutter-xfixes
+apk add fzf ripgrep fd
+apk add eza lf trash-cli htop stow
+apk add tar zip unzip 7zip lzip
+apk add yash yash-completion
+apk add neovim mpv nsxiv maim cmus
+#apk add imagemagick ffmpeg
+#apk add zathura zathura-pdf-poppler
+
+# Softwares
+apk add freetube librewolf
+apk add keepassxc nemo
+
+# Appearance
+apk add adwaita-icon-theme
+
+# Fonts
+apk add freetype-dev fontconfig-dev
+apk add font-iosevka-base
+apk add font-jetbrains-mono-nerd
+#apk add font-freefont font-roboto font-dejavu font-ubuntu
+#apk add font-noto font-noto-extra font-awesome-free
+#apk add font-linux-libertine font-liberation font-inconsolata
+
+# Add user to groups
+adduser yuu audio
+adduser yuu input
+adduser yuu plugdev
+
+# REBOOT
+reboot
---
+++ install-mint.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# Update system and repositories, add repos
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt-get update
+
+# Programming tools
+sudo apt-get install -y gcc build-essential gdb
+sudo apt-get install -y lua5.1 luarocks luajit
+sudo apt-get install -y python3 python3.12-venv python3-pip
+sudo apt-get install -y nodejs npm default-jre
+sudo apt-get install -y ninja-build gettext cmake ccache git
+
+# Cli tools
+sudo apt-get install -y wezterm tree-sitter-cli
+sudo apt-get install -y unzip zip tar lzip 7zip lf trash-cli eza
+sudo apt-get install -y htop stress-ng xwallpaper xclip
+sudo apt-get install -y fzf fd-find ripgrep jq
+sudo apt-get install -y yt-dlp mpv nsxiv imagemagick ffmpeg
+sudo apt-get install -y zathura zathura-poppler eyed3
+
+# Fonts and icons
+sudo apt-get install -y fonts-recommended fonts-noto
+sudo apt-get install -y fonts-freefont-otf fonts-roboto fonts-dejavu
+sudo apt-get install -y fonts-linuxlibertine fonts-liberation
+sudo apt-get install -y fonts-crosextra-carlito fonts-crosextra-caladea
+sudo apt-get install -y papirus-icon-theme
+
+# Software
+sudo apt-get install -y keepassxc gimp audacious extrepo
+sudo extrepo enable librewolf
+sudo apt update && sudo apt install librewolf -y
+
+### MacOS theme
+cd $HOME/Downloads
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1
+mkdir -p "$HOME/.local/share/themes"
+#./install.sh --dest $HOME/.local/share/themes --opacity solid --color dark --theme purple
+
+### Building neovim
+git clone https://github.com/neovim/neovim "$HOME/Downloads/neovim"
+#cd neovim && git checkout stable
+#make CMAKE_BUILD_TYPE=Release
+#sudo make install
+
+# Install fonts
+mkdir -p "$HOME/.local/share/fonts"
+cd "$HOME/.local/share/fonts"
+git clone https://github.com/sahibjotsaggu/San-Francisco-Pro-Fonts.git
+curl -L -o JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
+
+# Starship prompt
+#curl -sS https://starship.rs/install.sh | sh
+
+# Reboot
+sudo reboot
