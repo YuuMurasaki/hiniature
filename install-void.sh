cc1ee1c9c6089eb8dd16100cd0080571ac8a9c80
YuuMurasaki <hoangthibaongoc411@gmail.com>
2025-07-09 16:27:05 +0700

    ddm pipewire

--- install-void.sh
+++ install-void.sh
@@ -6,13 +6,21 @@
 # Setup RUNTIME: add this to /etc/pam.d/login at the end of 'session'
 # -session	optional	pam_rundir.so
 ###################################################################################
+# Setup pipewire
+# mkdir -p /etc/pipewire/pipewire.conf.d
+# ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
+# ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
+# mkdir -p /etc/alsa/conf.d
+# ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
+# ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+###################################################################################
 
 xbps-install -Syu
 xbps-install -y xorg xinit xrandr xsetroot xset xclip xwallpaper unclutter-xfixes
 xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel libXrandr-devel ncurses
 xbps-install -y freetype-devel fontconfig-devel
 xbps-install -y dbus pam_rundir
-xbps-install -y NetworkManager pipewire pipewire-devel libpipewire alsa-pipewire wireplumber rtkit
+xbps-install -y NetworkManager pipewire pipewire-devel rtkit alsa-pipewire wireplumber
 xbps-install -y git make curl wget patch
 xbps-install -y gcc tcc python lua luarocks LuaJIT 
 xbps-install -y opendoas stow htop tree-sitter
